922e66a22417860e97fc089c9b77abc2fe6e0d90
DyogoBendo <dyogoromagnabendo@gmail.com>
2022-01-21 08:32:18 -0300

    entrada e saida de valores com if

--- if_else.sh
+++ if_else.sh
@@ -1,9 +1,13 @@
 #!/bin/zsh
-T1="foo"
-T2="bar"
+
+echo Insert a value for the first variable
+read T1
+
+echo Insert a value for the second variable
+read T2
 
 if [ "$T1" = "$T2" ]; then
-    echo expression evaluated as true
+    echo both variables are equal
 else 
-    echo expression evaluated as false
+    echo the variables are different
 fi
